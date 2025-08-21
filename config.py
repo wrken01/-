c0c07e8f079ca8a59ddf78ae1503c0efee11f54d
wrken01 <wrken@foxmail.com>
2025-08-21 18:58:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+数据库连接配置
+可以根据公司实际数据库服务器信息修改
+"""
+DB_CONFIG = {
+    'host': 'localhost',      # MySQL 服务器 IP（建议内网 IP）
+    'user': 'root',        # MySQL 用户名
+    'password': '123456',      # MySQL 密码
+    'database': 'test',   # 要使用的数据库
+    'port': 3306
+
+}
